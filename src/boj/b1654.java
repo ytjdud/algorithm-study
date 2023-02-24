38bbad70593843b8d05e976094f24e4eeaa2537f
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-02-24 16:33:01 +0900

    upper bound 와 parametric search

---
+++ src/boj/b1654.java
@@ -0,0 +1,43 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// upper bound
+
+public class b1654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] LANs = new int[K];
+        long max = 0;
+        for(int i=0;i<K;i++) {
+            LANs[i] = Integer.parseInt(br.readLine());
+            if (max < LANs[i]) max = LANs[i];
+        }
+        max++;
+
+        long min =0, mid;
+        while(min<max){
+            mid = (min+max)/2;
+
+            int cnt=0;
+            for(int i=0;i<K;i++){
+                cnt += LANs[i]/mid ;
+            }
+
+            if(cnt < N){ // 랜선 개수 부족 == 더 짧게 잘라서 많이 만들어야함. == max 위치가 줄어야함
+                max = mid;
+            }else{ // 랜선 개수가 같거나 많음 == 더 길게 잘라서 적게 만들어야함 == min 위치가 올라야함
+                min = mid+1;
+            }
+        }
+
+        System.out.println(min-1);
+    }
+}
