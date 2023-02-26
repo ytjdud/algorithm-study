d4b7e7347ab542f6cf8319120822384a985f9195
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-02-26 22:32:27 +0900

    lower bound 설명 수정, #12738 추가

--- src/boj/b12015_12738.java
+++ src/boj/b12015_12738.java
@@ -7,10 +7,15 @@ import java.util.Arrays;
 
 /*
 최장 증가 수열 (LIS, Longest Increasing Subsequence)
-lower bound - ex) 1 2 3 6 7 9, put 4 가능한 모든 수(6 7 9) 중에서 제일 작은 수
+lower bound
+ex) 1 2 3 6 7 9, put 4 <- 3보다 큰 모든 수 중 가장 작은 수
+ex) -8 -7 -6 -5 -2 -1 put -3 <- -3 보다 큰 모든 수 중 가장 작은 수
+**lower bound : 찾고자 하는 값이 처음 나타나는 곳
+**upper bound : 찾고자 하는 값보다 큰 값이 처음 나타나는
  */
 
-public class b12015 {
+// 12015, 12738 코드 동일
+public class b12015_12738 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -18,7 +23,6 @@ public class b12015 {
 
         int[] LIS = new int[N];
         LIS[0] = A[0];
-//        LIS[1] = A[1]; 입력된 A[] 길이가 1 뿐 일 수도
         int len=1; // len of LIS[]
         for(int i=1;i<N;i++){ // i for A[]
             int v = A[i];
