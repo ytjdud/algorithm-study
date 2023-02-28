4040cf9dd7504c3f940383a4380cd8c694bcbb8e
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-03-01 00:30:14 +0900

    counting sort 로 풀어봤으나 큰 차이가 없음, 계산 부분에서 시간을 더 줄여야 하는듯

--- src/boj/b11399.java
+++ src/boj/b11399.java
@@ -4,18 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class b11399 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] P = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(P);
+//        int[] P = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+//        Arrays.sort(P);
+        int[] cs = new int[1001];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            cs[Integer.parseInt(st.nextToken())]++;
+        }
 
         int min =0;
-        for(int p:P){
-            min += N*p;
-            N--;
+//        for(int p:P){
+//            min += N*p;
+//            N--;
+//        }
+        for(int i=0;i<1001;i++){
+            while(cs[i]-- >0){
+                min += N*i;
+                N--;
+            }
         }
         System.out.println(min);
     }
