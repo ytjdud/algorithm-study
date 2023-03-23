54af0def7297d92195f43bbb6ecb113aebdfc545
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-03-23 16:48:26 +0900

    another idea -> KB(-) ms(-)

--- src/boj/b2195.java
+++ src/boj/b2195.java
@@ -3,9 +3,15 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+/*
+first
+p(subString of P) 전체에서 뒤에서 하나씩 지워가며 contains 확인
+145356KB	360ms
 
-// p(subString of P) 전체에서 뒤에서 하나씩 지워가며 contains 확인
-// 145356KB	360ms
+second
+앞에서 하나씩, 실제로 P 업데이트가 아닌 idx 로 자른척
+11912KB	92ms
+ */
 
 public class b2195 {
     static String str, P;
@@ -14,21 +20,16 @@ public class b2195 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         str = br.readLine();
         P = br.readLine();
+        int p_len = P.length();
         int cnt=0;
-        while(P.length() > 0){
-            int start = Math.min(P.length(), str.length());
-            P = get_s(start, P);
+        for(int i=0, start=0;i<p_len;i++){
+            if(str.contains(P.substring(start, i+1))) continue;
+            // 이제부터 not contains 이면
             cnt++;
-        }
-        System.out.println(cnt);
-    }
+            start = i;
 
-    private static String get_s(int start, String p) {
-        for(int i=start;i>=0;i--){
-            if(str.contains(p.substring(0,i))){
-                return p.substring(i);
-            }
         }
-        return "";
+        // 제일 마지막으로 잘린 나머지는 cnt 가 안됨
+        System.out.println(cnt+1);
     }
 }
